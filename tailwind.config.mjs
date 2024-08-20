9d03207be536e9d974d13c89ccf3431b1d6d621b
AresAndStuff <ares.dovalo@gmail.com>
2024-08-21 00:07:45 +0200

    improved themes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        "primary-dark": {
-          light: "#c5beb9",
-          DEFAULT: "#2b2935",
-          dark: "#2b2935",
-        },
-        primary: {
-          light: "#f4eee0",
-          DEFAULT: "#413d4d",
-          dark: "#413d4d",
-        },
-        neutral: {
-          DEFAULT: "#756e77",
-        },
-        "secondary-dark": {
-          light: "#2b2935",
-          DEFAULT: "#c5beb9",
-          dark: "#c5beb9",
-        },
-        secondary: {
-          light: "#413d4d",
-          DEFAULT: "#f4eee0",
-          dark: "#f4eee0",
-        },
+        primary_dark: 'var(--color_primary_dark)',
+        primary: 'var(--color_primary)',
+        neutral: 'var(--color_neutral)',
+        secondary_dark: 'var(--color_secondary_dark)',
+        secondary: 'var(--color_secondary)',
       },
     },
   },
